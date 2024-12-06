b7005017062fba293342d800baabf9132310b47a
CharlyZL <115099099+CharlyZL@users.noreply.github.com>
2024-12-05 18:56:55 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,6 @@ const urlsToCache = [
   'manifest.json',
 ];
 
-// Instalar el Service Worker
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -13,7 +12,6 @@ self.addEventListener('install', (event) => {
   );
 });
 
-// Activar el Service Worker
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
@@ -28,7 +26,6 @@ self.addEventListener('activate', (event) => {
   );
 });
 
-// Interceptar peticiones de red
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
